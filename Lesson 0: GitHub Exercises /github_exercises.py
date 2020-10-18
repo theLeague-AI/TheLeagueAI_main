3759db3cb10691d7b80be6687ee40d3e18507220
ngrajles1 <15281436+ngrajales1@users.noreply.github.com>
2020-10-18 16:41:27 -0500

    Replacing PRINT YOUR NAME HERE with my name

--- Lesson 0: GitHub Exercises /github_exercises.py
+++ Lesson 0: GitHub Exercises /github_exercises.py
@@ -3,4 +3,4 @@ This python file will be used to verify you have access to github and to start t
 look like. Pleas make sure to follow the instructions from the google doc.
 """
 
-print("PRINT YOUR NAME HERE")
+print("Nelson Grajales")
