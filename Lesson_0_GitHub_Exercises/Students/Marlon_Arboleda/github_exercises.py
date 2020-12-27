b443e46871de1f77beee758a1699ed403ac37c25
Marlon <marlon.arb3@gmail.com>
2020-12-26 22:07:07 -0600

    added name

---
+++ Lesson_0_GitHub_Exercises/Students/Marlon_Arboleda/github_exercises.py
@@ -0,0 +1,7 @@
+"""
+This python file will be used to verify you have access to github and to start teaching what a github workflow will
+look like. Pleas make sure to follow the instructions from the google doc.
+"""
+
+print("Marlon")
+
